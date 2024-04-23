ea16f97c3a508e9bbabe5149fcdd5f28f7942b92
Timur <sornowy@gmail.com>
2024-04-23 19:39:44 +0700

    debug

--- src/test/java/TextBoxTests.java
+++ src/test/java/TextBoxTests.java
@@ -8,18 +8,22 @@ import static com.codeborne.selenide.Selenide.open;
 public class TextBoxTests {
     @BeforeAll
     static void beforeAll() {
-        Configuration.browserSize = "1920x1080";
+        //Configuration.browserSize = "1920x1080";
+        //Configuration.browser = "firefox";
+        //System.setProperty("webdriver.firefox.bin", "C:\\Program Files\\Mozilla Firefox\\firefox.exe");
+        //System.setProperty("webdriver.gecko.driver", "C:\\WebDrivers\\geckodriver.exe");
         Configuration.baseUrl = "https://demoqa.com";
+        Configuration.timeout = 100;
     }
 
     @Test
     void fillFormTest() {
-        open("https://demoqa.com/text-box");
+        open("/text-box");
 
         $("#userName").setValue("Timur Timur");
         $("#userEmail").setValue("timur@gmail.com");
-        $("#currentAddress").setValue("Пушкина стрит");
-        $("#permanentAddress").setValue("Пушкин стрит_1");
+        $("#currentAddress").setValue("Some street");
+        $("#permanentAddress").setValue("Another street");
         $("#submit").click();
     }
 
