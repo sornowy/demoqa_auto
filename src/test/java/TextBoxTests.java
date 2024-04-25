889cf399d2c19797fca212fab426524834f22241
Timur <sornowy@gmail.com>
2024-04-25 20:50:34 +0700

    add Test for https://www.saucedemo.com/

--- src/test/java/TextBoxTests.java
+++ src/test/java/TextBoxTests.java
@@ -8,23 +8,33 @@ import static com.codeborne.selenide.Selenide.open;
 public class TextBoxTests {
     @BeforeAll
     static void beforeAll() {
-        //Configuration.browserSize = "1920x1080";
+        Configuration.browserSize = "1920x1080";
         //System.setProperty("webdriver.chromedriver", "C:\\WebDrivers\\chromedriver.exe");
-        Configuration.baseUrl = "https://demoqa.com";
+        //Configuration.baseUrl = "https://demoqa.com";
         //Configuration.baseUrl = "https://www.google.ru/";
-        //Configuration.timeout = 10000;
+        Configuration.timeout = 10000;
+        Configuration.holdBrowserOpen = true;
     }
 
     @Test
     void fillFormTest() {
-        open("https://ya.ru/");
+        open("https://www.saucedemo.com/");
 
-        $("#userName").setValue("Timur Timur");
-        //$("#text").setValue("Timur Timur");// Проверка работоспособности кода в Яндекс поиске:)
-        $("#userEmail").setValue("timur@gmail.com");
-        $("#currentAddress").setValue("Some street");
-        $("#permanentAddress").setValue("Another street");
-        $("#submit").click();
+
+        //Проверки для https://www.saucedemo.com/
+        $("#user-name").setValue("standard_user");
+        $("#password").setValue("secret_sauce");
+        $("#login-button").click();
+
+        //Проверка для demoqq
+//        $(".card mt-4 top-card").click();
+//        $("#item-0").click();
+//        $("#userName").setValue("Timur Timur");
+//        $("#text").setValue("Timur Timur");// Проверка работоспособности кода в Яндекс поиске:)
+//        $("#userEmail").setValue("timur@gmail.com");
+//        $("#currentAddress").setValue("Some street");
+//        $("#permanentAddress").setValue("Another street");
+//        $("#submit").click();
     }
 
 }
